ee92f6927c79590c9fa81687e1d240138198ea41
pipa <pipa.salfonzo@gmail.com>
2024-12-02 18:51:38 -0800

    part 2

--- day2/main.ts
+++ day2/main.ts
@@ -1,5 +1,6 @@
-const isLevelSafe = (list: number[]) => {
+const isLevelSafe = (list: number[], tolerance: number) => {
   let isAscending: boolean = false;
+  let tolerate = 0;
 
   for (let i = 0; i < list.length; i++) {
     if (i == 0) {
@@ -9,35 +10,50 @@ const isLevelSafe = (list: number[]) => {
       }
     }
     if (i + 1 == list.length) {
-      console.log("I am safe")
       return true;
     }
 
     if (isAscending) {
-      if (!(list[i] < list[i + 1] && (list[i+1] - list[i]) <= 3 )) {
+      if (!(list[i] < list[i + 1] && (list[i + 1] - list[i]) <= 3)) {
         //element is not okay
-        console.log(
-          `${list[i]} is not safe if ascending because ${
-            list[i + 1]
-          } cannot be! ${list[i] < list[i + 1]} ${(list[i+1] - list[i]) <= 3}`,
-        );
-        return false;
+        // console.debug(
+        //   `${list[i]} is not safe if ascending because ${
+        //     list[i + 1]
+        //   } cannot be! ${list[i] < list[i + 1]} ${
+        //     (list[i + 1] - list[i]) <= 3
+        //   }`,
+        // );
+        ++tolerate;
+        if (tolerate > tolerance) {
+          return false;
+        }
+
+        //Delete the element from arr and also decrease i to re-run
+        delete list[i--]
+        list = list.filter(l => l != undefined)
+
       }
     } else {
       if (!(list[i] > list[i + 1] && (list[i] - list[i + 1]) <= 3)) {
         //element is not okay
-        console.log(
-          `${list[i]} is not safe if not ascending because ${
-            list[i + 1]
-          } cannot be!  ${list[i] > list[i + 1]} ${list[i] - list[i + 1]}`,
-        );
-        return false;
+        // console.debug(
+        //   `${list[i]} is not safe if not ascending because ${
+        //     list[i + 1]
+        //   } cannot be!  ${list[i] > list[i + 1]} ${list[i] - list[i + 1]}`,
+        // );
+        ++tolerate;
+        if (tolerate > tolerance) {
+          return false;
+        }
+        //Delete the element from arr and also decrease i to re-run
+        delete list[i--]
+        list = list.filter(l => l != undefined)
       }
     }
   }
 };
 
-const solution = () => {
+const solution1 = () => {
   const inputText = new TextDecoder().decode(Deno.readFileSync("input.txt"));
 
   const reports = inputText.split("\n");
@@ -45,7 +61,7 @@ const solution = () => {
 
   reports.forEach((rep) => {
     let nums = rep.split(" ").map((num) => Number.parseInt(num));
-    if (isLevelSafe(nums)) {
+    if (isLevelSafe(nums, 0)) {
       safe++;
     }
   });
@@ -53,4 +69,21 @@ const solution = () => {
   console.log(`The number of safe reports is ${safe}`);
 };
 
-solution();
+const solution2 = () => {
+  const inputText = new TextDecoder().decode(Deno.readFileSync("input.txt"));
+
+  const reports = inputText.split("\n");
+  let safe = 0;
+
+  reports.forEach((rep) => {
+    let nums = rep.split(" ").map((num) => Number.parseInt(num));
+    if (isLevelSafe(nums, 1)) {
+      safe++;
+    }
+  });
+
+  console.log(`The number of safe reports is ${safe}`);
+};
+
+solution1();
+solution2();
