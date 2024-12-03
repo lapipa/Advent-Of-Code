0086a21b99807bb4217b9a4369047a28c1fa3d35
pipa <pipa.salfonzo@gmail.com>
2024-12-02 19:07:37 -0800

    day 1 pt 2

--- day1/main.ts
+++ day1/main.ts
@@ -32,4 +32,30 @@ const solution = () => {
   console.log(diff);
 };
 
+
+const solution2 = () => {
+  const td = new TextDecoder();
+  const input = Deno.readFileSync("input.txt");
+
+  const list1: number[] = [];
+  const list2: number[] = [];
+  let similar = 0
+
+  const inputText = td.decode(input);
+
+  const lines = inputText.split("\n");
+
+  lines.forEach((line) => {
+    const twoNums = line.split("   ");
+    list1.push(Number.parseInt(twoNums[0]));
+    list2.push(Number.parseInt(twoNums[1]));
+  });
+
+  list1.forEach(location => {
+    similar += list2.filter(loc => loc == location).length*location
+  })
+
+  console.log(`Similarity index is at ${similar}`)
+}
 solution();
+solution2();
