3f5196f4cd977d21330a7bda96c2cc6cebaf3444
pipa <pipa.salfonzo@gmail.com>
2024-12-08 12:14:29 -0800

    worked on c++ part , now to do wasm

--- day4/solution.cpp
+++ day4/solution.cpp
@@ -28,16 +28,18 @@ public:
             for (int y_delta = -1; y_delta <= 1; y_delta++)
             {
                 pair<int, int> coord(x_coord + x_delta, y_coord + y_delta);
-                if(coord.first < christmas.size() && coord.second < christmas[0].size()){
-                    if(coord.first >= 0 && coord.second >= 0){
+                if (coord.first < christmas.size() && coord.second < christmas[0].size())
+                {
+                    if (coord.first >= 0 && coord.second >= 0)
+                    {
                         coordinates_to_verify.push_back(coord);
-                    }   
+                    }
                 }
             }
         }
 
         for (int i = 0; i < coordinates_to_verify.size(); i++)
-        {   
+        {
             if (christmas[coordinates_to_verify[i].first][coordinates_to_verify[i].second] == lookingFor)
             {
                 direction_vectors.emplace_back(pair<int, int>(coordinates_to_verify[i].first - x_coord, coordinates_to_verify[i].second - y_coord)); // direction vector
@@ -46,9 +48,12 @@ public:
 
         return direction_vectors;
     }
-    auto isPointValid(pair<int,int> point){
-        if(point.first < grid.size() && point.second < grid[0].size()){
-            if(point.first >= 0 && point.second >=0 ){
+    auto isPointValid(pair<int, int> point)
+    {
+        if (point.first < grid.size() && point.second < grid[0].size())
+        {
+            if (point.first >= 0 && point.second >= 0)
+            {
                 return true;
             }
         }
@@ -57,60 +62,113 @@ public:
     }
     auto find_subword_direction(vector<vector<char>> &christmas, string word, int x_start, int y_start, pair<int, int> direction_vector)
     {
-        cout << "Subword around (" << x_start  << "," << y_start << ") " << endl;
+        // cout << "Subword around (" << x_start << "," << y_start << ") " << endl;
         for (int i = 0; i < word.length(); i++)
         {
             auto point_to_check = pair<int, int>(direction_vector.first * i + x_start, direction_vector.second * i + y_start);
-            if(!isPointValid(point_to_check)){
-                cout << "not valid , invalid point  (" << point_to_check.first  << "," << point_to_check.second << ") " << endl;
+            if (!isPointValid(point_to_check))
+            {
+                // cout << "not valid , invalid point  (" << point_to_check.first << "," << point_to_check.second << ") " << endl;
                 return false;
             }
-            cout << "Char " << christmas[point_to_check.first][point_to_check.second] << " looking for " << word[i] << endl;
+            // cout << "Char " << christmas[point_to_check.first][point_to_check.second] << " looking for " << word[i] << endl;
             if (christmas[point_to_check.first][point_to_check.second] != word[i])
             {
-                cout << "not valid" << endl;
+                // cout << "not valid" << endl;
                 return false;
             }
         }
         return true;
     }
 
-
     /**
      * Grab a specific x,y and count all the words found from this index
      */
     auto findWord(vector<vector<char>> &christmas, string word, int x_start, int y_start)
-    {   
-        cout << "Will try to find " << word << " around (" << x_start  << "," << y_start << ") " << endl;;
+    {
+        // cout << "Will try to find " << word << " around (" << x_start << "," << y_start << ") " << endl;
+        ;
         auto next = word[1];
         auto found = false;
         auto start = pair<int, int>(x_start, y_start);
         auto fromStartCount = 0;
-        if(christmas[x_start][y_start] != word[0]){
+        if (christmas[x_start][y_start] != word[0])
+        {
             // cout << "Found " << direction_vectors.size() << " clues!" << endl;
             return fromStartCount;
         }
         auto direction_vectors = lookAroundForClues(christmas, x_start, y_start, word[1]);
 
-        cout << "Found " << direction_vectors.size() << " clues!" << endl;
-        
+        // cout << "Found " << direction_vectors.size() << " clues!" << endl;
 
         /**
          * direction vector will always be equal to the amount of next char founds around the start indeces
          */
         for (int i = 0; i < direction_vectors.size(); i++)
-        {   
+        {
             auto dir_vec = direction_vectors[i];
             if (find_subword_direction(christmas, word.substr(1), x_start + dir_vec.first, y_start + dir_vec.second, dir_vec))
             {
                 fromStartCount++;
             }
         }
-         cout << "Found " << fromStartCount << " words!" << endl;
+        // cout << "Found " << fromStartCount << " words!" << endl;
         return fromStartCount;
     }
-};
 
+    auto find_crossed_xmas(vector<vector<char>> &christmas, int x_start, int y_start)
+    {
+
+        // use A pivot
+        if (christmas[x_start][y_start] != 'A')
+        {
+            return false;
+        }
+
+        // cout << "============== Will try to find crossed" << " around (" << x_start << "," << y_start << ") " << endl;
+        // cout << "pivot found" << endl;
+        auto s_clues = lookAroundForClues(christmas, x_start, y_start, 'S');
+        auto m_clues = lookAroundForClues(christmas, x_start, y_start, 'M');
+        auto crosses = 0;
+
+        if (m_clues.size() < 2 || s_clues.size() < 2)
+        {
+            // cout << "too low clues " << endl;
+            return false;
+        }
+
+        for (int i = 0; i < m_clues.size(); i++)
+        {
+            if (m_clues[i].first != 0 && m_clues[i].second != 0)
+            {
+                auto point_to_check = pair<int, int>(x_start + m_clues[i].first*-1,y_start + m_clues[i].second*-1 );
+                if (!isPointValid(point_to_check))
+                {
+                    // cout << "ERROR: not valid , invalid point  (" << point_to_check.first << "," << point_to_check.second << ") " << endl;
+                }
+                else
+                {
+
+                    // cout << "Found M clue at (" << x_start + m_clues[i].first << "," << y_start + m_clues[i].second << ") dir " << m_clues[i].first << "," << m_clues[i].second << endl;
+                    // cout << "checking out (" << point_to_check.first << "," << point_to_check.second << ")" << endl;
+                    if (christmas[point_to_check.first][point_to_check.second] == 'S')
+                    {   
+                        // cout << "Point checked out (" << point_to_check.first << "," << point_to_check.second << ")" << endl;
+                        crosses++;
+                    }
+                }
+            }
+        }
+
+        if (crosses == 2)
+        {
+            return true;
+        }
+
+        // cout << "ERROR: Only " << crosses << " crossed" << endl;
+        return false;
+    }
+};
 
 /**
  * This will be translated to deno code
@@ -133,15 +191,20 @@ int main()
     file.close();
 
     auto sol = Solution(rows);
-    auto allCounts = 0;
+    auto wordcounts = 0;
+    auto crossed_mas_count = 0;
     for (int i = 0; i < rows.size(); i++)
     {
         for (int j = 0; j < rows[i].size(); j++)
         {
             // cout <<  "row size: " << rows[i].size() << endl;
-            allCounts += sol.findWord(rows, "XMAS", i, j);
+            wordcounts += sol.findWord(rows, "XMAS", i, j);
+            if (sol.find_crossed_xmas(rows, i, j))
+            {
+                crossed_mas_count++;
+            }
         }
     }
-    cout << "Found " << allCounts << " XMAS :D" << endl;
-
+    cout << "Found " << wordcounts << " XMAS :D" << endl;
+    cout << "Found " << crossed_mas_count << " XMAS :D" << endl;
 }
